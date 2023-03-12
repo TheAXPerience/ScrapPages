968bcfa2e71c4a1f45a204012f656d552e2f4a9d
Alvin Xu <alvin3xu1@gmail.com>
2023-03-12 16:09:17 -0700

    merge

--- scrappages/scrappages/settings.py
+++ scrappages/scrappages/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-27=g(3nvyey3v-cpo-#pqh^ry8p!!y16%pvhqc3xx(qq9bny&0'
+SECRET_KEY = config('DSK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
