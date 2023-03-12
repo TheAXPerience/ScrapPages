ccf20eb2b81883d43732e92fcf3a503fc647c65e
Alvin Xu <alvin3xu1@gmail.com>
2023-03-12 16:13:39 -0700

    just change the secret key in production, the one here is temporary during dev

--- scrappages/scrappages/settings.py
+++ scrappages/scrappages/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DSK')
+# this will be changed in production
+SECRET_KEY = 'django-insecure-27=g(3nvyey3v-cpo-#pqh^ry8p!!y16%pvhqc3xx(qq9bny&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
