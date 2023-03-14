810d05e57054203e8bd29bb8671bf486d5b2df59
Alvin Xu <alvin3xu1@gmail.com>
2023-03-13 20:45:13 -0700

    add commented info about User

--- scrappages/profiles/models.py
+++ scrappages/profiles/models.py
@@ -5,8 +5,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 def profile_picture_path(instance, filename):
     # MEDIA_ROOT/prof_pics/id__filename
-    return f"prof_pics/{instance.user.username}__{filename}"
+    return f"prof_pics/{instance.user.get_username()}__{filename}"
 
+
+# User class
+# username - max_length = 150, characters = alphanumeric and "_@+.-"
+# password - stored as a hash
+# email
+# first_name - max_length = 150
+# last_name - max_length = 150
+
+# User.check_pasword(raw_password)
+# User.set_password(raw_password)
+# User.set_unusable_password() - use for "deleted" user account
+# User.get_username()
+
+# there are more fields and methods for permissions, but every user here is gonna have basic ones so
 
 class Profile(models.Model):
     user = models.OneToOneField(
